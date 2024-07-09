039b4040cc2ba5e5e72f67a6ca9dbee469f6062e
nikhilkj1404 <nikhilkj0414@gmail.com>
2024-07-09 13:42:16 +0530

    Update StringCalculator.cpp

--- StringCalculator.cpp
+++ StringCalculator.cpp
@@ -2,16 +2,22 @@
 #include <sstream>
  
 int StringCalculator::add(const std::string& input) {
-    if (input.empty() || input == "0") {
+    if (isEmptyOrZero(input)) {
         return 0;
     }
+    return sumofNumbers(input);
+}
+
+bool StringCalculator::isEmptyOrZero(const std::string& input) {
+    return input.empty() || input == "0";
+}
  
+int StringCalculator::sumOfNumbers(const std::string& input) {
     std::istringstream stream(input);
     std::string number;
     int sum = 0;
-
     while (std::getline(stream, number, ',')) {
-     sum += std::stoi(number);
+        sum += std::stoi(number);
     }
-    return sum; 
+    return sum;
 }
