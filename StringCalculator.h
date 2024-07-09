eb0899eae7fc72f104bc4c76ac0f1ee6d706676e
nikhilkj1404 <nikhilkj0414@gmail.com>
2024-07-09 10:22:52 +0530

    Update StringCalculator.h

--- StringCalculator.h
+++ StringCalculator.h
@@ -1,3 +1,11 @@
-int add(const char* input) {
-  return -1;
-}
+#ifndef STRINGCALCULATOR_H
+#define STRINGCALCULATOR_H
+ 
+#include <string>
+ 
+class StringCalculator {
+public:
+    int add(const std::string& input);
+};
+ 
+#endif // STRINGCALCULATOR_H
