2259562c52a2388cf0f3ec5d97962a28911ab92d
nikhilkj1404 <nikhilkj0414@gmail.com>
2024-07-09 10:54:42 +0530

    Update StringCalculator.cpp

--- StringCalculator.cpp
+++ StringCalculator.cpp
@@ -1,7 +1,7 @@
 #include "StringCalculator.h"
  
 int StringCalculator::add(const std::string& input) {
-    if (input.empty()) {
+    if (input.empty() || input == "0") {
         return 0;
     }
     return 0; 
